d40a81dbc4875ff27bc8df85fcbdc1853a5525d0
Joao Marcelino <dev.marcelino@hotmail.com>
2025-09-19 00:29:09 -0300

    assets

---
+++ etiquetas/src/app/loading.tsx
@@ -0,0 +1,8 @@
+
+export default function Loading() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <span className="text-lg animate-pulse text-gray-600">Carregando aplicação...</span>
+    </div>
+  );
+}
