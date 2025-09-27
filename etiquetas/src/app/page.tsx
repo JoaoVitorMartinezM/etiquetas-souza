c7fe8d80000f029c9023a38b4fbfadaa26838ab2
Joao Marcelino <dev.marcelino@hotmail.com>
2025-09-27 20:06:55 -0300

    Ajusta elementos da página home para visualização mobile

--- etiquetas/src/app/page.tsx
+++ etiquetas/src/app/page.tsx
@@ -2,27 +2,46 @@ import ServiceHomeCard from "@/components/cards/Services/home";
 import { Caption } from "@/components/tophograph/Caption";
 import Subtitle from "@/components/tophograph/Subtitle";
 import { Button } from "@/components/ui/button";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col h-auto">
-      <div className="flex p-4 md:px-80 items-end justify-center al h-[400px] bg-[url(/imagem-principal-home.png)] bg-top md:bg-center">
+      <div className="flex p-4 md:px-80 items-end justify-center al h-[400px] bg-[url(/imagem-principal-home.png)] bg-top bg-no-repeat bg-cover md:bg-center">
         <Link href={"/servicos"}>
           <Button className="mb-28 bg-orange-500 font-bold">Conheça Nossos Serviços!</Button>
         </Link>
       </div>
       <section>
-        
+
       </section>
 
       <section className="flex justify-around items-center flex-wrap bg-slate-200 py-8">
-        <div className="flex flex-col items-start my-8 w-1/4">
+        <div className="flex flex-col items-center my-8 w-full p-4 md:w-1/4">
           <Subtitle title="Serviços" />
           <Caption text="Nós temos 12 anos de excelência, estamos prontos para atender a sua necessidade." />
         </div>
-        <div className="flex flex-wrap gap-4 justify-center md:justify-start">
+        <Carousel className="w-full max-w-xs md:hidden self-center flex justify-center">
+          <CarouselContent>
+            <CarouselItem className="flex justify-center">
+              <ServiceHomeCard name="Etiquetas Especiais" description="Etiquetas personalizadas" icon={'/icone-etiqueta-premium.png'} />
+            </CarouselItem>
+            <CarouselItem className="flex justify-center">
+              <ServiceHomeCard name="Agilidade" description="Entrega Rápida" icon={'/icone-agilidade.png'} />
+            </CarouselItem>
+            <CarouselItem className="flex justify-center">
+              <ServiceHomeCard name="Alta Demanda" description="Entregamos independente do volume" icon={'/icone-alto-volume.png'} />
+            </CarouselItem>
+            <CarouselItem className="flex justify-center">
+              <ServiceHomeCard name="Qualidade" description="Impressões de alta definição" icon={'/icone-impressao.png'} />
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious className="-left-6" />
+          <CarouselNext className="-right-2" />
+        </Carousel>
+        <div className="hidden md:flex flex-wrap gap-4 justify-center md:justify-start">
           <ServiceHomeCard name="Etiquetas Especiais" description="Etiquetas personalizadas" icon={'/icone-etiqueta-premium.png'} />
           <ServiceHomeCard name="Agilidade" description="Entrega Rápida" icon={'/icone-agilidade.png'} />
           <ServiceHomeCard name="Alta Demanda" description="Entregamos independente do volume" icon={'/icone-alto-volume.png'} />
@@ -30,23 +49,23 @@ export default function Home() {
         </div>
         {/* <Image alt="Máquina de etiquetas"  src='/maquina.webp' width={400} height={200}/> */}
       </section>
-      <section className="flex flex-col items-start my-8 gap-4 w-1/2 mx-auto">
+      <section className="flex flex-col items-start my-8 gap-4 md:w-1/2 mx-auto">
         <Subtitle title="Produtos em Destaque" />
-        <div className="flex justify-around items-center w-full">
+        <div className="flex flex-col gap-4 md:flex-row md:justify-around items-center w-full">
           <Link href={"/produtos"}>
-            <Image alt="Etiquetas para alimentos" src='/products/rotulo_alimenticio.png' width={200} height={300} className="rounded-full" />
+            <Image alt="Etiquetas para alimentos" src='/products/rotulo_alimenticio.png' width={200} height={300} className="md:rounded-full w-full rounded-md" />
             <div className="text-center">
               <Caption text="Rótulos alimentícios." />
             </div>
           </Link>
           <Link href={"/produtos"}>
-            <Image alt="Etiquetas BOPP" src='/products/rotulo_bopp.webp' width={200} height={300} className="rounded-full" />
+            <Image alt="Etiquetas BOPP" src='/products/rotulo_bopp.webp' width={200} height={300} className="md:rounded-full w-full rounded-md" />
             <div className="text-center">
               <Caption text="Etiquetas BOPP." />
             </div>
           </Link>
           <Link href={"/produtos"}>
-            <Image alt="Etiquetas metalizadas" src='/products/rotulo-metalizado.jpg' width={200} height={300} className="rounded-full" />
+            <Image alt="Etiquetas metalizadas" src='/products/rotulo-metalizado.webp' width={400} height={300} className="md:rounded-full w-80 rounded-md" />
             <div className="text-center">
               <Caption text="Etiquetas Metalizadas." />
             </div>
